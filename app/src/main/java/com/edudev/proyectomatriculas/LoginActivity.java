3ecb901925a97047f80a246d7c816a8ac80c212b
eduardo0419 <daniel_19_unt@hotmail.com>
2017-01-11 14:46:51 -0500

    Diseño de menu principal

--- app/src/main/java/com/edudev/proyectomatriculas/LoginActivity.java
+++ app/src/main/java/com/edudev/proyectomatriculas/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.edudev.proyectomatriculas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button ingresa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ingresa=(Button)findViewById(R.id.btn_ingresa);
+        ingresa.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_ingresa:
+                startActivity(new Intent(this,MenuActivity.class));
+                break;
+        }
     }
 }
