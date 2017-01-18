ea5ac85c02c6579109258147fa095a76a78ec44d
eduardo0419 <daniel_19_unt@hotmail.com>
2017-01-18 13:48:53 -0500

    Corregido error de tabla cursos

--- app/src/main/java/com/edudev/proyectomatriculas/io/response/ListarResponse.java
+++ app/src/main/java/com/edudev/proyectomatriculas/io/response/ListarResponse.java
@@ -1,11 +1,12 @@
 package com.edudev.proyectomatriculas.io.response;
 
 import com.edudev.proyectomatriculas.io.model.Curso;
+import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
 
 public class ListarResponse {
-
+    @SerializedName("cursos")
     private ArrayList<Curso> arrayList;
 
     public ArrayList<Curso> getArrayList() {
